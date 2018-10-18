c3c26ff086b69ec63e215fc9ccda4218badf395f
SosninRoman <sosninroman92@gmail.com>
2018-10-19 00:55:26 +0500

    Добавлен конфигурационный файл для travis

--- split.cpp
+++ split.cpp
@@ -8,6 +8,7 @@ std::vector<std::string> split(const std::string &str, char d)
 
     auto stop = str.find_first_of(d);
     decltype(stop) start = 0;
+
     while(stop != std::string::npos)
     {
         r.emplace_back(str.substr(start, stop - start));
